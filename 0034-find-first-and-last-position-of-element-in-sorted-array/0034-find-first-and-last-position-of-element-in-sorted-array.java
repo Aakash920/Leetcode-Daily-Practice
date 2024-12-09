364550521b73e9525e62b0ad6c92b11e116402f9
Aakash Singh <84664685+Aakash920@users.noreply.github.com>
2024-12-10 03:39:55 +0530

    Time: 0 ms (100%), Space: 45.8 MB (57.95%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,38 +1,62 @@
 class Solution {
-    int[] ans={-1,-1};
+    // int[] ans={-1,-1};
     public int[] searchRange(int[] nums, int target) {
-        if(nums.length==0) return ans;
-        bs1(nums, target);
-        bs2(nums, target);
+        int[] ans={-1,-1};
+        // if(nums.length==0) return ans;
+        // bs1(nums, target);
+        // bs2(nums, target);
+        ans[0]=bs(nums, target, true);
+        ans[1]=bs(nums, target, false);
         return ans;
     }
-    public void  bs1(int[] arr, int t){
-        int s=0, e=arr.length-1;
+//     public void  bs1(int[] arr, int t){
+//         int s=0, e=arr.length-1;
+//         while(s<=e){
+//             int mid=s+(e-s)/2;
+//             if(t<arr[mid]){
+//                 e=mid-1;
+//             }else{
+//                 s=mid+1;
+//             }
+//         }
+
+//         if(e>=0 && e<arr.length && arr[e]==t){
+//             ans[1]=e;
+//         }
+//     }
+//     public void  bs2(int[] arr, int t){
+//         int s=0, e=arr.length-1;
+//         while(s<=e){
+//             int mid=s+(e-s)/2;
+//             if(t>arr[mid]){
+//                 s=mid+1;
+//             }else{
+//                 e=mid-1;
+//             }
+//         }
+//         if(s>=0 && s<arr.length && arr[s]==t){
+//             ans[0]=s;
+//         }
+// }
+    private int bs(int[] arr, int t, boolean findFirstIdx){
+         int s=0, e=arr.length-1;
+         int ans=-1;
         while(s<=e){
             int mid=s+(e-s)/2;
             if(t<arr[mid]){
                 e=mid-1;
-            }else{
+            }else if(t>arr[mid]){
                 s=mid+1;
+            }else{
+                //potential ans
+                ans=mid;
+                if(findFirstIdx){
+                    e=mid-1;
+                }else{
+                    s=mid+1;
+                }
             }
-        }
-
-        if(e>=0 && e<arr.length && arr[e]==t){
-            ans[1]=e;
-        }
     }
-    public void  bs2(int[] arr, int t){
-        int s=0, e=arr.length-1;
-        while(s<=e){
-            int mid=s+(e-s)/2;
-            if(t>arr[mid]){
-                s=mid+1;
-            }else{
-                e=mid-1;
-            }
-        }
-        if(s>=0 && s<arr.length && arr[s]==t){
-            ans[0]=s;
-        }
+    return ans;
 }
 }
