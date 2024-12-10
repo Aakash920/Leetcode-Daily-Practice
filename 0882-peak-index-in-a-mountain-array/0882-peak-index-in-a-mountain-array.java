d79f63296a5545486dbde3dfe443326410b3eba1
Aakash Singh <84664685+Aakash920@users.noreply.github.com>
2024-12-11 02:46:09 +0530

    Time: 0 ms (100%), Space: 56.3 MB (48.75%) - LeetHub

--- 0882-peak-index-in-a-mountain-array/0882-peak-index-in-a-mountain-array.java
+++ 0882-peak-index-in-a-mountain-array/0882-peak-index-in-a-mountain-array.java
@@ -6,6 +6,10 @@ class Solution {
             if(arr[mid]<arr[mid+1]){
                 s=mid+1;
             }else if(arr[mid]>arr[mid+1]){
+                // why we are not doing e=mid-1
+                // coz we didn't know that the previous index has greater value or not
+                // e.g- {1,2,5,6,4,3,2} so, suppose  we are at 3 but we don't know about that
+                // before 3 that is 4 is greater or not.
                 e=mid;
             }
         }
