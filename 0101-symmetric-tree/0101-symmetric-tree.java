0de1b7e2e916e8772e4fcd4ab13b7f91f62a736f
Aakash Singh <84664685+Aakash920@users.noreply.github.com>
2024-12-29 17:08:15 +0530

    Time: 1 ms (13.82%), Space: 41.8 MB (58%) - LeetHub

--- 0101-symmetric-tree/0101-symmetric-tree.java
+++ 0101-symmetric-tree/0101-symmetric-tree.java
@@ -15,16 +15,43 @@
  */
 class Solution {
     public boolean isSymmetric(TreeNode root) {
-        if(root==null) return false;
-
-        return check(root.left,root.right);
+        // if(root==null) return false;
+        return BFS(root.left,root.right);
+        // return check(root.left,root.right);
     }
-    public boolean check(TreeNode l, TreeNode r){
-        if(l==null && r==null) return true;
-        if(l==null || r==null) return false;
-        if(l.val!=r.val){
-            return false;
+    //DFS Approach
+    // public boolean check(TreeNode l, TreeNode r){
+    //     if(l==null && r==null) return true;
+    //     if(l==null || r==null) return false;
+    //     if(l.val!=r.val){
+    //         return false;
+    //     }
+    //     return check(l.left,r.right) && check(l.right,r.left);
+    // }
+
+    public boolean BFS(TreeNode l, TreeNode r){
+        Queue<TreeNode> q=new LinkedList<>();
+        q.add(l);
+        q.add(r);
+        while(!q.isEmpty()){
+            TreeNode lside=q.poll();
+            TreeNode rside=q.poll();
+            if(lside==null && rside==null) {
+                continue;
+            }
+
+            if(lside==null || rside==null){
+                return false;
+            }
+            
+            if(lside.val!=rside.val){
+                return false;
+            }
+            q.offer(lside.left);
+            q.offer(rside.right);
+            q.offer(lside.right);
+            q.offer(rside.left);
         }
-        return check(l.left,r.right) && check(l.right,r.left);
+        return true;
     }
 }
